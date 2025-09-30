1d034f2c95bbc6091f490161e490152924593dae
Jeff Kelly <9146472+jekelly@users.noreply.github.com>
2025-09-29 23:38:38 -0700

    Add se/*xml passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 module.exports = function (eleventyConfig) {
   // necessary to automatically prepend the prefix to internal links
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+  eleventyConfig.addPassthroughCopy("se/*.xml");
   return {
     // prefix used by GitHub Pages, usually your project name
     pathPrefix: "/jekelly.github.io/"
